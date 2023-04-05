1226b81e056438b774723046391162dc284ee1cd
limberMay <limbermay1@gmail.com>
2023-04-05 00:53:19 -0600

    versión 0.0.1 lista

--- test/components/ProductTitle.test.tsx
+++ test/components/ProductTitle.test.tsx
@@ -1,13 +1,36 @@
-import * as React from 'react';
+import * as React from 'react'
+import { create } from 'react-test-renderer'
+import { ProductTitle, ProductCard } from '../../src/components';
+import { product1 } from '../data/products';
 
-import { create } from 'react-test-renderer';
-import {ProductTitle} from "../../src";
+
+
 describe('ProductTitle', () => {
-    test('Should show the component with the custom title', () => {
-        const wrapper = create(
-            <ProductTitle title="Custom Product" />
-        );
+    
+    test('debe de mostrar el componente correctamente con el titulo personalizado', () => {
 
-        expect(wrapper.toJSON()).toMatchSnapshot();
+        const wrapper = create(
+            <ProductTitle title="Custom Product" className="custom-class" />
+        )
+        
+        expect( wrapper.toJSON() ).toMatchSnapshot();
+        
     });
-});
+
+
+    test('debe de mostrar el componente con el nombre del producto', () => {
+        
+        const wrapper = create(
+            <ProductCard product={ product1 }>
+                {
+                    () => (
+                        <ProductTitle />
+                    )
+                }
+            </ProductCard>
+        )
+        
+        expect( wrapper.toJSON() ).toMatchSnapshot();
+    })
+})
+
---
+++ test/data/products.ts
@@ -0,0 +1,11 @@
+export const product1 = {
+    id: '1',
+    title: 'Product title',
+    // img: '../assets/img.png',
+}
+
+export const product2 = {
+    id: '1',
+    title: 'Product title',
+    img: '../assets/img.png',
+}
